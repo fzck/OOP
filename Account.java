f0cbfa0147910637a62ac81989a38452efecec58
FloAt <ohmj101@gmail.com>
2016-09-20 15:54:01 +0800

    made edits for readability purposes

--- Account.java
+++ Account.java
@@ -1,7 +1,6 @@
 public class Account{
 	private int accountNumber;
 	private double balance;
-
 	public Account (int n, double b){
 		this(n);
 		setBalance(b);
@@ -9,12 +8,9 @@ public class Account{
 	public Account (int acc) {
 		this.accountNumber = acc;
 	}
-
-
 	public int getAccountNumber(){
 		return accountNumber;
 	}
-	
 	public double getBalance(){
 		return balance;
 	}
@@ -22,26 +18,20 @@ public class Account{
 		if(balance < 0.0) {
          // error!
          throw new IllegalArgumentException("Broke af");
-      }
-		this.balance = balance;
+      	}
+      	this.balance = balance;
 	}
-
 	public void credit(double amount){
 		setBalance(balance + amount);
-
 	}
-
 	public void debit(double amount){
 		if (balance >= amount){
 			setBalance(balance - amount);
 		} else {
 		// error!
          throw new IllegalArgumentException("amount withdrawn exceeds the curernt balance!");
-
 		}
-
 	}
-
 	public String toString(){
 		String str = String.format("A/C no:%d, Balance=$%.2f",accountNumber,balance);
 		return str;
